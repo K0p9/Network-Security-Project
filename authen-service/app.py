8f4b15537342b8414627d22715c2c58f29609e99
Kop9 <21522573@gm.uit.edu.vn>
2024-05-16 21:15:59 +0700

    first commit

---
+++ authen-service/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app_auth = Flask(__name__)
+app_auth.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app_auth.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app_auth.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")  # Change this to a secure secret key
+jwt = JWTManager(app_auth)
+db = SQLAlchemy(app_auth)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), default='user', nullable=False)
+
+@app_auth.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        # Generate an access token
+        access_token = create_access_token(identity=user.username, additional_claims={'role': user.role})
+        return jsonify({'access_token': access_token}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+@app_auth.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists'}), 400
+
+    # Set the role to 'modder' for the new user
+    role = data.get('role', 'user')  # Default to 'user' if 'role' is not provided
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'), role=role)
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'Registration successful', 'role': new_user.role}), 201
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app_auth.run(host='0.0.0.0', port=5001, debug=True)
